3b2803c8d5f661386fe8cfefe9a6eb2f004c9fc1
yan <yan.bernardo24@hotmail.com>
2023-05-04 16:20:52 -0300

    Troca de senha simplificada
    Agora é possível alterar somente o nome da rede ou somente a senha na alteração de senha TLI

--- js/trocasenha.js
+++ js/trocasenha.js
@@ -4,8 +4,10 @@ function gerarScriptNomeESenha() {
     let nomeRede = document.getElementById('nome').value;
     let senhaRede = document.getElementById('senha').value;
     let output = document.getElementById("txtarea");
-    
-    let script = `DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-4;
+    let script;
+
+    if (nomeRede.length != 0 && senhaRede.length != 0) {
+        script = `DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-4;
 DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-5;
 DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-6;
 DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-7;
@@ -14,7 +16,27 @@ ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}
 ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-5::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.PreSharedKey.1.PreSharedKey,PARAMVALUE="${senhaRede}";
 ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-6::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.5.SSID,PARAMVALUE="${nomeRede}-5G";
 ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-7::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.5.PreSharedKey.1.PreSharedKey,PARAMVALUE="${senhaRede}";
-`
+        `;
+    } else if (nomeRede.length != 0 && senhaRede.length == 0) {
+        script = `DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-4;
+DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-6;
+
+ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-4::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.SSID,PARAMVALUE="${nomeRede}";
+ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-6::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.5.SSID,PARAMVALUE="${nomeRede}-5G";
+        `
+    } else if (nomeRede.length == 0 && senhaRede.length != 0) {
+        script = `DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-5;
+DLT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-7;
+
+ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-5::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.PreSharedKey.1.PreSharedKey,PARAMVALUE="${senhaRede}";
+ENT-HGUTR069-SPARAM::HGUTR069SPARAM-1-1-${posicao.getSlot()}-${posicao.getPon()}-${posicao.getPorta()}-7::::PARAMNAME=InternetGatewayDevice.LANDevice.1.WLANConfiguration.5.PreSharedKey.1.PreSharedKey,PARAMVALUE="${senhaRede}";
+        `
+    } else {
+        return window.alert("Favor preencher pelo menos 2 campos!")
+    }
+
+
+    
 
     output.value = script;
     let copyButton = document.getElementById("copy");
